76bc80eae4c0fb6dd7b50109e4a25be50cc7db46
Tom Hardy <tj.hardy77@yahoo.co.uk>
2018-10-04 15:12:41 +0100

    version 0.01
    some basic code added to main

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+#include <tomsengine/tomsengine.h>
+#include <memory>
+
+#define shared std::shared_ptr
+#define weak std::weak_ptr
+
+using namespace tomsengine;
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+	// Initialize the engine
+	shared<Core> core = Core::initialize();
+
+    // Create an in-game object
+	shared<Entity> entity = core->addEntity();
+	
+	// Add a simple component to it
+	weak<TestScreen> testScreen = entity->addComponent<TestScreen>();
+
+	// Start the engine loop
+	core->start();
+
 	return 0;
 }
