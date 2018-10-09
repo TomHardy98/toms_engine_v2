ac58e4f8c7d4dbe2853b90b063efd28f4a092ebb
Tom Hardy <tj.hardy77@yahoo.co.uk>
2018-10-09 13:06:35 +0100

    Created VAO and VBO buffers

--- src/tomsengine/Core.cpp
+++ src/tomsengine/Core.cpp
@@ -2,15 +2,76 @@
 
 bool Core::Initialize()
 {
-	// Set up OpenGL and SDL inside of here using Lab 3
-
 	int WINDOW_WIDTH = 1200;
 	int WINDOW_HEIGHT = 800;
 
 	SDL_Window *window = SDL_CreateWindow("Game Window", SDL_WINDOWPOS_UNDEFINED, 
 	SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
 
+	if (!SDL_GL_CreateContext(window))
+	{
+		throw std::exception();
+	}
 
+	if (glewInit() != GLEW_OK)
+	{
+		throw std::exception();
+	}
+
+	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	SDL_GL_SwapWindow(window);
+
+	// Creating a triangle
+
+	const GLfloat positions[] = {
+		0.0f, 0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f
+	};
+
+	GLuint positionsVboId = 0;
+
+	// Create a new VBO on the GPU and bind it
+	glGenBuffers(1, &positionsVboId);
+
+	if (!positionsVboId)
+	{
+		throw std::exception();
+	}
+
+	glBindBuffer(GL_ARRAY_BUFFER, positionsVboId);
+
+	// Upload a copy of the data from memory into the new VBO
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+	// Reset the state
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	GLuint vaoId = 0;
+
+	// Create a new VAO on the GPU and bind it
+	glGenVertexArrays(1, &vaoId);
+
+	if (!vaoId)
+	{
+		throw std::exception();
+	}
+
+	glBindVertexArray(vaoId);
+
+	// Bind the position VBO, assign it to position 0 on the bound VAO and flag it to be used
+	glBindBuffer(GL_ARRAY_BUFFER, positionsVboId);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *)0);
+
+	glEnableVertexAttribArray(0);
+
+	// Reset the state
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	// NEED TO ADD VERTEX AND FRAGMENT SHADER
 
 	std::cout << "Hello" << std::endl;
 	return true;
