f1650b2f8d222bdba008195583605575cd3b1ebd
Tom Hardy <tj.hardy77@yahoo.co.uk>
2018-10-15 14:28:34 +0100

    added safe main with exception

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,4 +1,5 @@
 #include <memory>
+#include <exception>
 #include <iostream>
 #include <tomsengine/tomsengine.h>
 #include <tomsengine/Core.h>
@@ -6,24 +7,33 @@
 #define shared std::shared_ptr
 #define weak std::weak_ptr
 
-//using namespace tomsengine;
-
-int main()
+void safe_main()
 {
-	
 	// Initialize the engine
 	shared<Core> core;
 	core->Initialize();
 
-    /*// Create an in-game object
+	/*// Create an in-game object
 	shared<Entity> entity = core->addEntity();
-	
+
 	// Add a simple component to it
 	weak<TestScreen> testScreen = entity->addComponent<TestScreen>();
 
 	// Start the main engine loop
 	core->start();
 	*/
+}
+
+int main()
+{
+	try
+	{
+		safe_main();
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
 
 	system("PAUSE");
 
