ef63bbe08c937eab202868693810b0c81bce522b
Tom Hardy <tj.hardy77@yahoo.co.uk>
2018-10-20 16:10:03 +0100

    vertex buffer and array added

---
+++ src/tomsengine/VertexBuffer.h
@@ -0,0 +1,23 @@
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+
+#include <vector>
+
+namespace tomsengine
+{
+	class VertexBuffer
+	{
+		GLuint id;
+		int components;
+		std::vector<GLfloat> data;
+		bool dirty;
+
+	public:
+		VertexBuffer();
+		void add(glm::vec3 value);
+		void add(glm::vec4 value);
+		int getComponents();
+		int getDataSize();
+		GLuint getId();
+	};
+}
