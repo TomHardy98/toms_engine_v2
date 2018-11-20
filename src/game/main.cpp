d0812eebe426e180f75ca780dd7a3de3bbc1fd55
Tom Hardy <tj.hardy77@yahoo.co.uk>
2018-11-20 14:27:47 +0000

    Back face culling added

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -29,8 +29,8 @@ void safe_main()
 	entity->addComponent<BoxCollider>();   // Adding a box collider to entity
 
 	entity->getComponent<Transform>()->Translate(0.0f, 0.0f, 0.0f);   // Move entity component using transform component
-	entity->getComponent<Transform>()->Rotate(8.0f, 0.0f, 0.0f);   // Rotate entity component using transform component
-	entity->getComponent<Transform>()->Scale(0.5f, 0.5f, 0.5f);
+	entity->getComponent<Transform>()->Rotate(0.0f, 0.0f, 0.0f);   // Rotate entity component using transform component
+	entity->getComponent<Transform>()->Scale(0.0f, 0.0f, 0.0f);
 
 	shared<Audio> au = std::make_shared<Audio>("../data/audio/dixie_horn.ogg");   // Add an audio clip
 
